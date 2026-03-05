279dcb541cdbd9e7fc01e0924ad7e2c5fa12b686
Naing Khant Win <naingkhantab@gmail.com>
2026-03-05 16:58:32 +0630

    mail

--- config/mail.php
+++ config/mail.php
@@ -40,7 +40,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             // Support both Laravel 11 MAIL_SCHEME and older/common MAIL_ENCRYPTION env names.
-            'scheme' => env('MAIL_SCHEME', env('MAIL_ENCRYPTION')),
+            'scheme' => env('MAIL_SCHEME', 'smtp'),
+
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
