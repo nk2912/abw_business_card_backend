07c9d55cc4dbf222cce66eba22020d6add1d7ca5
Naing Khant Win <naingkhantab@gmail.com>
2026-03-06 10:27:11 +0630

    start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+echo "==> Preparing Laravel cache directories..."
+mkdir -p \
+  storage/framework/views \
+  storage/framework/cache \
+  storage/framework/sessions \
+  bootstrap/cache
+
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
 echo "==> Running migrations..."
 php artisan migrate --force
 
